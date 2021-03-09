c9c3e4f31b35b408e88d7166751304642c3a3e50
Julia Martyn <52349144+juliamartyn@users.noreply.github.com>
2021-03-09 10:36:26 +0200

    Added Kafka Consumer

---
+++ KafkaConsumer/src/main/java/com/juliamartyn/service/KafkaConsumer.java
@@ -0,0 +1,20 @@
+package com.juliamartyn.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class KafkaConsumer {
+    private static final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    private static final String TOPIC = "topic1";
+
+    @KafkaListener(topics = TOPIC, groupId = "group_id")
+    public void messageListener(String message) {
+        logger.info(String.format("Consumed message -> %s", message));
+    }
+
+}
