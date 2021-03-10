c1da6b66fc9afad3a8e0790433781bcbea220bf9
Julia Martyn <52349144+juliamartyn@users.noreply.github.com>
2021-03-10 10:45:55 +0200

    Update KafkaProducer.java

--- KafkaProducer/src/main/java/com/juliamartyn/service/KafkaProducer.java
+++ KafkaProducer/src/main/java/com/juliamartyn/service/KafkaProducer.java
@@ -17,7 +17,7 @@ public class KafkaProducer {
     private KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        logger.info(String.format("Producing message -> %s", message));
+        logger.info("Producing message -> {}", message);
         this.kafkaTemplate.send(TOPIC, message);
     }
 }
